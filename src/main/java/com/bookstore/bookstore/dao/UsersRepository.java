c7d63f594450af0c50d155a995f6e8f723861786
git-test <rockwithmadan@gmail.com>
2021-10-18 12:28:36 +0545

    backend-setup

---
+++ src/main/java/com/bookstore/bookstore/dao/UsersRepository.java
@@ -0,0 +1,17 @@
+package com.bookstore.bookstore.dao;
+
+import com.bookstore.bookstore.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+    @Query("SELECT u FROM Users u join u.roles r WHERE u.username = :username and r.roleName='USER'")
+    public List<Users> findUser(@Param("username") String username);
+
+    @Query("SELECT u FROM Users u join u.roles r WHERE u.username = :username and r.roleName='ADMIN'")
+    public List<Users> findAdmin(@Param("username") String username);
+}
